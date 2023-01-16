531a2491b9ab7e4d731506ee0b35aac6ffb1f364
vinitakaware-FST <112160102+vinitakaware-FST@users.noreply.github.com>
2023-01-16 13:07:03 +0530

    Update streamlit_app.py
    added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+steamlit.text('Hard-Boiled Free-Range Egg')
