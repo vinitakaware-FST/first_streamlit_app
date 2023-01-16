00f0951b94eb65508db206f7d44151f714c2dd8c
vinitakaware-FST <112160102+vinitakaware-FST@users.noreply.github.com>
2023-01-16 13:13:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's new Healthy Diner')
+streamlit.title('My Moms new Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
